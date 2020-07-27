4957d5646a7e0aa91e890151ae86add7577bb8a0
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-07-27 14:09:46 +0000

    added ability to save to students.csv

--- directory.rb
+++ directory.rb
@@ -1,30 +1,79 @@
-#Let's put all the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon",cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+@students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit #this will cause the program to terminate
+    else 
+      puts "I don't know what you mean, try again"
+  end  
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
 end
-def print(students)
-  students.each do |student|
+
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+
+
+interactive_menu
